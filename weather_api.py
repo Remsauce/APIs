e50125e2d0318ce06f6adfb243309808da9b3ef2
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-22 17:55:50 +0300

    Weather API with amended oustring output formatting

--- weather_api.py
+++ weather_api.py
@@ -1,19 +1,22 @@
-import urllib2, urllib, json
+import urllib2, json
 
 city_names = ['Nairobi', 'Lagos', 'London']
 app_key = '2bc3e79bb974a007818864813f53fd35'
+url = 'http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric'
+
+print "City".ljust(15), "Temperature".ljust(15), "Description"
+print '='*50  
 
 def get_weather():
-
+	
 #loops through city names list and passes it to weather api url. 
 #data is opened, read and passed to result.
 #json presents the data as a dictionary. Pass this to Postman for easier reading
-	for city in city_names:    
-		url = 'http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric'\
-		.format(city, app_key)
-		result = urllib2.urlopen(url).read()
-		data = json.loads(result)		
-		print "City: ", city, "\n", "Temperature: ", data['main']['temp'],\
-		 "\n", "Description: ", data['weather'][0]['description']
 
-get_weather()
+	for city in city_names:    
+		result = urllib2.urlopen(url.format(city, app_key)).read()
+		data = json.loads(result) 
+		print city.ljust(15), str(data['main']['temp']).ljust(15), data['weather'][0]['description']
+
+			
+get_weather()			
